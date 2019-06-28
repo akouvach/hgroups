2b32286e7c51fa49baf04486b551dce645066584
Andrés Kouvach <akouvach@yahoo.com>
2019-06-28 19:57:17 -0300

    inicial

---
+++ server.js
@@ -0,0 +1,131 @@
+'use strict';
+ 
+/*
+1.- Instalar NodeJs desde https://nodejs.org/en/
+
+2.- Crear un directorio (por ejemplo "www" ) para colocar 
+los arhivos estáticos :
+- html
+- js
+- css
+- imagenes
+
+3.- Instalar los paquetes para utilizar el servidor
+y conectar con la base de datos access:
+
+Desde la línea de comandos (cmd) en el directorio de trabajo, 
+ejecutar:
+
+npm init
+npm install body-parser --save
+npm install express --save
+npm install node-adodb --save
+
+4.- Ejecuto el servidor web
+
+node server.js
+*/
+
+
+const ADODB = require('node-adodb');
+const connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=data.mdb;');
+
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const dotenv = require('dotenv')
+dotenv.config();
+
+console.log(process.env.DB_PASSWORD) // baconpancakes
+console.log(process.env.DB_USER) // root
+
+/*
+Dentro del servidor web existen las rutas... estas rutas son las 
+diferentes urls.  Por ejemplo:
+localhost/
+localhost/insertar
+localhost/buscar
+localhost/insertar/usuarios
+
+
+Estas rutas me ayudan a ordenar qué funciones ejecutar cuando se pide algo
+
+Las peticiones al servidor (realizadas a través de las diferentes rutas) 
+puedern ser de varios tipos:
+
+GET: Se utilizan para pedir datos
+POST: Se utilizan para enviar datos
+DELETE: Se utilizan para enviar datos destinados a eliminar
+
+*/
+
+
+
+//Esta es una petición de enviar datos 
+// y esta direccionada a la ruta "/insertar"
+
+app.post("/insertar", (req, res, next) => {
+
+  var registro = req.body;
+  var instruccion = "Insert into usuarios (nombre, apellido, email, pais) values ('" + registro.nombre + 
+  "','" + registro.apellido + "','" + registro.email + "','" + registro.pais + "')";
+
+  //Voy a insertar lo que me mandaron por parámetro
+    connection
+    .execute(instruccion)
+    .then(data => {
+        console.log(JSON.stringify(data, null, 2));
+    })
+    .catch(error => {
+      res.render(error);
+    });
+
+    res.json({mensaje:"insercion exitoas"});  
+
+
+});
+
+// esta es una petición de borrado
+app.delete("/borrarTodo", (req, res, next) => {
+
+  var instruccion = "delete * from usuarios";
+
+  //Voy a insertar lo que me mandaron por parámetro
+    connection
+    .execute(instruccion)
+    .then(data => {
+        console.log(JSON.stringify(data, null, 2));
+    })
+    .catch(error => {
+      res.render(error);
+    });
+
+    res.json({mensaje:"borrado exitoso"});  
+
+
+});
+
+// esta es una petición para solicitar información
+app.get("/usuarios", (req, res, next) => {
+    connection
+    .query("SELECT * FROM Usuarios")
+    .then(data => {
+        res.json(data);
+    })
+    .catch(error => {
+      res.render(error)
+    });
+});
+
+//Le indico donde estan mis archivos estáticos
+// y el nombre del archivo por default que quiero que muestre:
+// datos.html
+app.use("/",express.static('www',{index:"datos.html"}));
+
+app.listen(process.env.PORT, () => {
+ console.log("Server running on port ",process.env.PORT);
+});
---
+++ www/datos.js
@@ -0,0 +1,110 @@
+let desarrollo = true;
+
+function obtenerBase(){
+    let port="";
+    let url=""
+    if(desarrollo){
+        port="5000";
+        url = "http://localhost:" + port; 
+    } else {
+        port="5000";
+        url = "https://hgroups.herokuapps.com:" + port; 
+    }
+    return (url);
+}
+let base = obtenerBase();
+
+
+function mostrarJson(obj){
+
+
+    var lugar=document.getElementById("resultados");
+    var mensaje = "<div class='w3-responsive'>";
+
+    mensaje += "<table class='w3-table-all'>";
+
+    mensaje += "<caption>Usuarios del sistema</caption>";
+
+    // hago un ciclo para colocar los titulos
+    let titulos = obj[0];
+    mensaje += "<thead><tr>";
+    for(var j in titulos){
+        mensaje += "<th>" + j + "</th>";
+    }
+    mensaje += "</tr></thead>";
+
+    mensaje+="<tbody>";
+    
+    //ahora recorro todos los elementos del vector para poner los datos
+    for (var i in obj) {
+        mensaje += "<tr>";
+        if(obj.hasOwnProperty(i)){
+            let miObj = obj[i];
+            for(var j in miObj){
+                mensaje += "<td>" + miObj[j] + "</td>";
+            }
+        } 
+        mensaje += "</tr>";
+    }
+
+    mensaje+="</tbody>";
+
+    mensaje += "</table></div>";
+
+    lugar.innerHTML = mensaje;
+}
+
+
+function mostrarUsuarios(){
+    var url = base + "/usuarios";
+    var misCabeceras = new Headers();
+
+    console.log(url);
+    var miInit = { method: 'GET',
+                headers: misCabeceras,
+                mode: 'cors',
+                cache: 'default' };
+
+    fetch(url,miInit)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(miJson) {
+        mostrarJson(miJson);
+        
+    });
+}
+
+function agregarUsuario(){
+
+    var url = base + "/insertar";
+    var data = {nombre: 'pepe',apellido:'gonzalez',email:'gonzalea@delpueblo.com',pais:'Argento'};
+
+    fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+        console.log('Success:', response);
+
+    });
+
+}
+
+function borrarTodo(){
+    var url = base + "/borrarTodo";
+
+    fetch(url, {
+        method: 'DELETE', // or 'PUT'
+        body: "" // data can be `string` or {object}!
+
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+        console.log('Success:', response);
+    });
+}
